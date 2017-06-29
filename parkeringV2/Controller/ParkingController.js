67a6a21f903acd6dbd14b821e8936d077a770bb3
Kasper Ottzen <kasper@ottzen.com>
2017-06-29 21:53:47 +0200

    edited service and controller files

--- parkeringV2/Controller/ParkingController.js
+++ parkeringV2/Controller/ParkingController.js
@@ -1,5 +1,5 @@
 var parkingApp = angular.module("parkingApp", []);
 
-parkingApp.controller('ParkingCtrl', function($scope, ParkingService) {
-	$scope = ParkingService;
- });
+parkingApp.controller('ParkingCtrl', ['$scope', 'ParkingService', function($scope, ParkingService) {
+    $scope.items = ParkingService.parkingData;
+}]);	
--- parkeringV2/Services/ParkingService.js
+++ parkeringV2/Services/ParkingService.js
@@ -1,8 +1,9 @@
 var parkingApp = angular.module("parkingApp", []);
 
-parkingApp.service('ParkingService', function($http){
+parkingApp.service('ParkingService', function($http) {
     $http.get("http://www.odaa.dk/api/action/datastore_search?resource_id=2a82a145-0195-4081-a13c-b0e587e9b89c").then(function (response) {
-                	//console.log(response.data.result.records)
-                    return response;                 
-                });
+    	//console.log(response.data.result.records);
+    	var parkingData = response.data.result.records;
+        return parkingData;                 
+    });
  });
